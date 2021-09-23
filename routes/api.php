60aada03b6f1616ab80695454cc5450b50359bfc
nicocadq <nmachado.camejo@gmail.com>
2021-09-22 21:14:15 -0300

    Fix login route

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::resource('donations', DonationController::class);
 
 // Login and Register public Routes
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', ['as' => 'login', 'uses' => 'AuthController@login']);
 
 
 // Protected Routes
