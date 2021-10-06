7eea6828997a371e13bb65bc5368eb2c9e338e04
nicocadq <nmachado.camejo@gmail.com>
2021-10-06 13:17:32 -0300

    Fix attach user to petition in the admin

--- app/Http/Controllers/Admin/PetitionCrudController.php
+++ app/Http/Controllers/Admin/PetitionCrudController.php
@@ -63,6 +63,7 @@ class PetitionCrudController extends CrudController
 
         CRUD::field('subject');
         CRUD::field('description');
+        CRUD::field('user_id');        
         CRUD::field('state');
 
         /**
