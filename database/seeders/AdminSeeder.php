d24de898c366871d652d671849e5c5dbcf70ed70
alejandroGonGon <alejandro.gonzalez@anima.edu.uy>
2021-10-25 08:44:36 -0300

    arreglando y añadiendo las seeders a un admin ara tener uno por defecto creado

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -13,6 +13,10 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('admin')->insert([
+            'name' => 'Administrador',
+            'email' => env('ADMIN_EMAIL'),
+            'password' => env('ADMIN_PASSWORD'),
+        ]);
     }
 }
