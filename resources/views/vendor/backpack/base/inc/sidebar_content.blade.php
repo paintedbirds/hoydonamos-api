5492b8c25edaedc529704ebc08155298385456cf
alejandroGonGon <alejandrogongon.me@gmail.com>
2021-12-09 19:59:23 -0300

    eliminando Backpack Y agreguando Orchid

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-question'></i> Users</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('petition') }}'><i class='nav-icon la la-question'></i> Petitions</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('donation') }}'><i class='nav-icon la la-question'></i> Donations</a></li>
-
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('donation-request') }}'><i class='nav-icon la la-question'></i> Donation requests</a></li>
