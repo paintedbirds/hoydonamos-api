e0f663ef5441a53fc0a526429561bedbb2058144
Alejandro <56725406+alejandroGonGon@users.noreply.github.com>
2021-12-09 07:38:22 -0300

    Update resources/lang/es/passwords.php
    Co-authored-by: Nicolas Machado <nmachado.camejo@gmail.com>

--- resources/lang/es/passwords.php
+++ resources/lang/es/passwords.php
@@ -16,5 +16,5 @@ return [
     'sent'      => 'Le hemos enviado por correo electrónico el enlace para restablecer su contraseña',
     'throttled' => 'Por favor espere antes de intentar de nuevo.',
     'token'     => 'El token de restablecimiento de contraseña es inválido.',
-    'user'      => 'No encontramos ningún usuario con ese correo electrónico.',
+    'user'      => 'No encontramos ningún usuario asociado a este correo electrónico',
 ];
