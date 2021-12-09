df2b77409905a51c7c95acdc8e59c7661d1024a9
Alejandro <56725406+alejandroGonGon@users.noreply.github.com>
2021-12-09 07:37:56 -0300

    Update resources/lang/es/passwords.php
    Co-authored-by: Nicolas Machado <nmachado.camejo@gmail.com>

--- resources/lang/es/passwords.php
+++ resources/lang/es/passwords.php
@@ -12,7 +12,7 @@
 */
 
 return [
-    'reset'     => '¡Su contraseña ha sido restablecida!',
+    'reset'     => '¡Su contraseña ha sido restablecida exitosamente!',
     'sent'      => '¡Le hemos enviado por correo electrónico el enlace para restablecer su contraseña!',
     'throttled' => 'Por favor espere antes de intentar de nuevo.',
     'token'     => 'El token de restablecimiento de contraseña es inválido.',
