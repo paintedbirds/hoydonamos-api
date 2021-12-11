85314182186ed62982480dd059613141bdc0702f
alejandroGonGon <alejandrogongon.me@gmail.com>
2021-12-11 20:00:05 -0300

    adding the petitions list screen

--- app/Models/Petition.php
+++ app/Models/Petition.php
@@ -5,18 +5,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Orchid\Screen\AsSource;
+use Orchid\Filters\Filterable;
 
 
 class Petition extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, AsSource, Filterable;
     
     protected $fillable = [
         'subject',
         'description',
         ];
     protected $with = ['user'];
-    
+
+    protected $allowedFilters = [
+        'subject',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $allowedSorts = [
+        'name',
+    ];
     /**
      * Functions of each model
      * 
---
+++ app/Orchid/Screens/PetitionInfoScreen.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Orchid\Screens;
+
+use App\Models\Petition;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Orchid\Screen\Fields\Input;
+use Orchid\Screen\Fields\Relation;
+use Orchid\Screen\Fields\Upload;
+use Orchid\Support\Facades\Layout;
+use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Screen;
+use Orchid\Support\Facades\Alert;
+use Orchid\Screen\Sight;
+use Orchid\Screen\TD;
+use Orchid\Support\Facades\Toast;
+use Orchid\Screen\Fields\Select;
+
+class PetitionInfoScreen extends Screen
+{
+    /**
+     * Display header name.
+     *
+     * @var string
+     */
+    public $name = 'Ver una Peticion';
+
+    /**
+     * Display header description.
+     *
+     * @var string
+     */
+    public $description = 'Toda la informacion sobre una Peticion creada.';
+
+    /**
+     * @var bool
+     */
+    /**
+     * Query data.
+     *
+     * @param petition $petition
+     *
+     * @return array
+     */
+    public function query(Petition $petition): array
+    {
+        return [
+            'petition' => $petition
+        ];
+    }
+
+    /**
+     * Button commands.
+     *
+     * @return Link[]
+     */
+    public function commandBar(): array
+    {
+        return [
+
+            Button::make('Guardar cambios')
+                ->icon('pencil')
+                ->method('UpdateState'),
+
+            Button::make('Delete')
+                ->icon('trash')
+                ->method('remove'),
+
+        ];
+    }
+
+    /**
+     * Views.
+     *
+     * @return Layout[]
+     */
+    public function layout(): array
+    {
+        return [
+             Layout::legend('petition', [
+                Sight::make('id')->popover('Numero identificativo de la donacion en el sistema'),
+                Sight::make('description')->popover('Descripción de la donacion'),
+                Sight::make('state')->popover('Estado de la donacion'),
+                Sight::make('created_at')->popover('Fecha de creacion de la donacion'),
+            ]),
+            Layout::rows([
+                Select::make('petition.state')
+                    ->options([
+                        'pending' => 'PENDING',
+                        'rejected' => 'REJETCTED',
+                        'published' => 'PUBLISHED',
+                    ])->title('Estado')
+                    ->help('Cambia el estado de una donacion')
+            ]),
+            Layout::legend('petition.user', [
+                Sight::make('id')->popover('Numero identificativo del usuario que creo la donacion en el sistema'),
+                Sight::make('name')->popover('Nombre del usuario'),
+                Sight::make('email')->popover('Correo electronico del usuario'),
+            ]),
+        ];
+    }
+
+    /**
+     * @param Petition    $petition
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function UpdateState(Petition $petition, Request $request)
+    {
+        $petition->fill($request->get('petition'))->save();
+        Alert::info('Has actualizado correactamente el estado!');
+    }
+
+    /**
+     * @param petition $petition
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Exception
+     */
+    public function remove(Petition $petition)
+    {
+        $petition->delete();
+
+        Alert::info('Has eliminado la Petition exitosamente');
+
+        return redirect()->route('platform.petition.list');
+    }
+}
