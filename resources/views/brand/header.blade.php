d2a630d88e081a5466deafd97a0460016783682a
alejandroGonGon <alejandrogongon.me@gmail.com>
2021-12-15 12:19:08 -0300

    agregando el header al menu

---
+++ resources/views/brand/header.blade.php
@@ -0,0 +1,14 @@
+@push('head')
+    <link
+        href="/favicon.ico"
+        id="favicon"
+        rel="icon"
+    >
+@endpush
+
+<p class="h2 n-m font-thin v-center">
+    <span class="m-l d-none d-sm-block">
+        Panel Administrativo
+        <small class="v-top opacity">hoydonamos</small>
+    </span>
+</p>
