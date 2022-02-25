351d31d3aba843004f8a2a8e4bc1e2d3172b1aca
Alejandro <56725406+alejandroGonGon@users.noreply.github.com>
2022-02-24 22:48:17 -0300

    Update app/Http/Requests/UpdateFormRequest.php
    Co-authored-by: Ruben Derigo <68236987+rubenderigo@users.noreply.github.com>

--- app/Http/Requests/UpdateFormRequest.php
+++ app/Http/Requests/UpdateFormRequest.php
@@ -38,7 +38,7 @@ class UpdateFormRequest extends FormRequest
             'password.required' => 'La contraseña es requerida',
             'name.required' => 'El nombre es requerido',
             'name.string' => 'Indique un nombre valido',
-            'phone.numeric' => 'Porfavor indique un número',
+            'phone.numeric' => 'Por favor indique un número',
             'about_me.string' => 'Porfavor indique solo texto',
             'image.image' => 'No se pudo subir la imagen',
             'image.max' => 'El tamaño máximo de la imagen puede ser de hasta 1Mb',
